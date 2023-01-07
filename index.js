c700ab4131579a865ac27303bc1d51e5aa8ac7f3
samrat <samratojha16@gmail.com>
2023-01-07 16:05:28 +0545

    -added year and date

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+exports.default = void 0;
 var _react = require("react");
 const styles = {
   marginLeft: "5px",
@@ -10,6 +14,44 @@ const styles = {
   cursor: "pointer",
   backgroundColor: "red",
 };
+const yearLabel = [
+  {
+    index: 259,
+    label: 5,
+  },
+  {
+    index: 519,
+    label: 10,
+  },
+  {
+    index: 779,
+    label: 15,
+  },
+  {
+    index: 1039,
+    label: 20,
+  },
+  {
+    index: 1299,
+    label: 25,
+  },
+  {
+    index: 1819,
+    label: 35,
+  },
+  {
+    index: 2599,
+    label: 50,
+  },
+  {
+    index: 3379,
+    label: 65,
+  },
+  {
+    index: 4159,
+    label: 80,
+  },
+];
 function MomentoMori({
   boxColorOption = ["#4ea69d", "#b350aa", "#a5c45a", "#a1405d"],
 }) {
@@ -31,12 +73,14 @@ function MomentoMori({
     const weeks = [];
     for (let i = 0; i < 4160; i++) {
       let color;
+      let date = start.toString();
       if (start <= end && start.getTime() <= end.getTime()) {
         color = boxColor;
       } else {
         color = "white";
       }
       weeks.push({
+        date: date,
         color: color,
       });
       start.setDate(start.getDate() + 7);
@@ -59,7 +103,8 @@ function MomentoMori({
         {
           style: {
             marginLeft: "15px",
-            marginTop: "20px",
+            marginTop: "30px",
+            width: "80vw",
           },
         },
         /*#__PURE__*/ React.createElement(
@@ -94,7 +139,7 @@ function MomentoMori({
             /*#__PURE__*/ React.createElement(
               "i",
               null,
-              " Momento Mori Calender"
+              " Momento Mori Life Calender"
             )
           ),
           /*#__PURE__*/ React.createElement(
@@ -122,25 +167,53 @@ function MomentoMori({
       /*#__PURE__*/ React.createElement(
         "div",
         {
-          className: "calender mt-4",
+          className: "calender",
         },
         week.map((item, index) => {
           let sixMonth = index % 26 === 0;
           let rowSpace = (index + 1) % 520 === 0;
+          console.log(item.date);
           return /*#__PURE__*/ React.createElement(
             React.Fragment,
             null,
             /*#__PURE__*/ React.createElement(
               "div",
               {
+                title: item.date !== "Invalid Date" && item.date.slice(4, 15),
                 className: "week-cell",
                 style: {
                   marginBottom: rowSpace && "10px",
-                  marginLeft: sixMonth && "10px",
+                  marginLeft: sixMonth && "8px",
                   backgroundColor: item.color,
                 },
               },
-              " "
+              index % 52 === 0 &&
+                /*#__PURE__*/ React.createElement(
+                  "h6",
+                  {
+                    style: {
+                      fontSize: "10px",
+                      marginLeft: "-60px",
+                    },
+                  },
+                  item.date !== "Invalid Date" && item.date.slice(4, 15)
+                ),
+              " ",
+              yearLabel.map((year) => {
+                if (year.index === index)
+                  return /*#__PURE__*/ React.createElement(
+                    "h6",
+                    {
+                      style: {
+                        fontSize: "14px",
+                        marginLeft: "15px",
+                        fontWeight: "bold",
+                        marginTop: "-3px",
+                      },
+                    },
+                    year.label
+                  );
+              })
             )
           );
         })
@@ -148,4 +221,5 @@ function MomentoMori({
     )
   );
 }
-module.exports = MomentoMori;
+var _default = MomentoMori;
+exports.default = _default;
